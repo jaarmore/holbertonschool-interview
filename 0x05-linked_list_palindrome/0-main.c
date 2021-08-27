e4cb3909c362617061e8256476599948c912f906
jaarmore <jaarmore@gmail.com>
2021-08-26 20:53:19 -0500

    adding files to repository

---
+++ 0x05-linked_list_palindrome/0-main.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *current;
+	int i;
+
+	head = NULL;
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 1);
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	head = NULL;
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 1);
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	head = NULL;
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 1);
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	head = NULL;
+	add_nodeint_end(&head, 1);
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	head = NULL;
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	head = NULL;
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 1);
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	head = NULL;
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	head = NULL;
+	print_listint(head);
+
+	if (is_palindrome(NULL) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	head = NULL;
+	for (i = 0; i < 1001; i++)
+		add_nodeint_end(&head, i);
+	for (i = 1000; i >= 0; i--)
+		add_nodeint_end(&head, i);
+
+	current = head;
+	for (i = 0; i < 727; i++)
+		current = current->next;
+	current->n = -98;
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	head = NULL;
+	for (i = 0; i < 1001; i++)
+		add_nodeint_end(&head, i);
+	for (i = 1000; i >= 0; i--)
+		add_nodeint_end(&head, i);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	return (0);
+}
---
+++ 0x05-linked_list_palindrome/README.md
@@ -0,0 +1,22 @@
+# 0x05. Linked list palindrome
+
+Write a function in C that checks if a singly linked list is a palindrome.
+
++ Prototype: `int is_palindrome(listint_t **head);`
++ Return: `0` if it is not a palindrome, `1` if it is a palindrome
++ An empty list is considered a palindrome.
+
+
+## Requirements
+
++ Allowed editors: `vi, vim, emacs`
++ All your files will be compiled on `Ubuntu 14.04 LTS`
++ Your programs and functions will be compiled with `gcc 4.8.4` using the flags `-Wall -Werror -Wextra and -pedantic`
++ All your files should end with a new line
++ Your code should use the `Betty` style. It will be checked using `betty-style.pl` and `betty-doc.pl`
++ You are not allowed to use global variables
++ No more than 5 functions per file
+
+
+### AUTHOR
+**JACKSON MORENO**
