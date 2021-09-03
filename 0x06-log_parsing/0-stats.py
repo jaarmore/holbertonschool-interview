64a24728a4f0e4eed61c49b4b7c7b1bb0a615cc2
jaarmore <jaarmore@gmail.com>
2021-09-02 20:23:16 -0500

    fix code on file

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,47 +1,59 @@
 #!/usr/bin/python3
-"""Parses a log"""
-from sys import stdin
+"""
+Script that reads stdin line by line and computes metrics:
+- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+                code> <file size>
+- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+    statistics from the beginning:
+Example:
+    File size: 5213
+    200: 2
+    401: 1
+    403: 2
+    404: 1
+    405: 1
+    500: 3
+"""
+import sys
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+summ = 0
 
-fs = -1
-sc = -2
+
+def prn_stats():
+    """
+    Function that print stats about log
+    """
+    global summ
+
+    print('File size: {}'.format(summ))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
+
 
 if __name__ == "__main__":
-    def printstat():
-        """Prints the file size and the status codes along with occurences"""
-        print("File size:", file_size)
-        for k, v in sorted(stat.items()):
-            if v:
-                print("{}: {}".format(k, v))
-
-    def parselog(file_size):
-        """Increments stat and file_size based on contents of line
-
-        Args:
-            file_size: current file size
-
-        Returns:
-            file_size
-        """
-        arr = line.split()
-        try:
-            file_size += int(arr[fs])
-            if arr[sc] in stat:
-                stat[arr[sc]] += 1
-        except Exception:
-            pass
-        return file_size
-
+    cnt = 0
     try:
-        file_size = 0
-        count = 1
-        stat = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
-                "405": 0, "500": 0}
-        for line in stdin:
-            file_size = parselog(file_size)
-            if count % 10 == 0:
-                printstat()
-            count += 1
-        printstat()
+        """ Iter the standar input """
+        for data in sys.stdin:
+            try:
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """ If there is a lenght """
+                summ += int(fact[-1])
+            except:
+                pass
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                prn_stats()
+                cnt = 0
     except KeyboardInterrupt:
-        printstat()
+        prn_stats()
         raise
+    else:
+        prn_stats()
