7465480d3684cc9273f382368ff9c9cdd4e7765d
jaarmore <jaarmore@gmail.com>
2021-11-16 19:42:37 -0500

    Adding solution files to repository

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Calculate how much water will be retained after it rains
+"""
+
+
+def maxWater(arr, n):
+    """
+    Function that identifies how much water can be stored
+    """
+    # To store the maximum water
+    # that can be stored
+    max_water = 0
+
+    # For every element of the array
+    for i in range(1, n - 1):
+        # Find the maximum element on its left
+        left = arr[i]
+        for j in range(i):
+            left = max(left, arr[j])
+
+        # Find the maximum element on its right
+        right = arr[i]
+        for j in range(i + 1, n):
+            right = max(right, arr[j])
+
+        # Update the maximum water
+        max_water = max_water + (min(left, right) - arr[i])
+
+    return max_water
+
+
+def rain(walls):
+    """
+    Function that calculate how much water will be retained after it rains.
+
+    Arg:
+        - walls: is a list of non-negative integers.
+
+    Return: Integer indicating total amount of rainwater retained.
+    """
+    water_retained = maxWater(walls, len(walls))
+    return water_retained
